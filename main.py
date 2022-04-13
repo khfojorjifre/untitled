4be825cc49dad0d541ff56aa486d4fbfd6290363
khfojorjifre <103474939+khfojorjifre@users.noreply.github.com>
2022-04-13 14:03:43 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,9 @@
+basic.show_icon(IconNames.STICK_FIGURE)
+music.play_melody("C D E F F E D C ", 120)
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+""")
